344332400de9eba2be15b21cb0ca206fa2baa0ea
Nicholas David Boucher <nboucher@college.harvard.edu>
2016-12-24 02:46:41 +0000

    Added basic security key

--- application.py
+++ application.py
@@ -232,10 +232,14 @@ def new_grant():
     # Escape all ampersands in query string that don't seem relevant
     # (Unfortunately, Qualtrisc doesn't do this for us)
     raw_data = request.query_string.decode('utf8').split('&')
+    # tuple of valid query keys (adding 'k' for security key)
+    grant_fields = list(vars(Grant).keys())
+    grant_fields.append('k')
+    valid_queries = tuple(grant_fields)
     parsed_args = []
     for arg in raw_data:
         # Skip first param, check if if arg starts with acceptable field
-        if len(parsed_args) == 0 or arg.startswith(tuple(vars(Grant).keys())):
+        if len(parsed_args) == 0 or arg.startswith(valid_queries):
             parsed_args.append(arg)
         else:
             # append argument to previous arg and escapse '&'
@@ -243,6 +247,11 @@ def new_grant():
     # Rebuild query string and parse as if normal
     clean_query = "&".join(parsed_args)
     args = parse_qs(clean_query)
+    
+    # Verify security key
+    sec_key = Config.query.filter_by(key='security_key').first().value
+    if not args.get('k') or sec_key != args.get('k')[0]:
+        return "Invalid Security Key. You do not have access to this system."
     
     # Get Next Grant ID
     current_week = Config.query.filter_by(key='grant_week').first()

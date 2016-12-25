c48b9bb5feb6a774ce2992c20276dd2d0f29a5a7
Nicholas David Boucher <nboucher@college.harvard.edu>
2016-12-25 22:00:50 +0000

    Refactored query string parsing; Updated Readme

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 New Online-system for Vetting Applications
 
 Nova is an web application written in Python/Flask that will handle all UC grant applications, from application creation to fund dispersement.
+
+
+## Implementation TODO
+
+1. Add query string redirect URL to Grant Application Survey
+2. Add query string redirect URL to CPF Survey
+3. Modify CPF Survey to accept Grant ID as Embedded Value from Query String
--- helpers.py
+++ helpers.py
@@ -1,3 +1,26 @@
 def usd(value):
     """Formats value as USD."""
     return "${:,.2f}".format(value)
+    
+def get_grant_args(query_string):
+    """ Gets arguments specific to application from query string,
+        escaping some troublsesome characters """
+    
+    # Escape all ampersands in query string that don't seem relevant
+    # (Unfortunately, Qualtrisc doesn't do this for us)
+    raw_data = query_string.decode('utf8').split('&')
+    # tuple of valid query keys (adding 'k' for security key)
+    grant_fields = list(vars(Grant).keys())
+    grant_fields.append('k')
+    valid_queries = tuple(grant_fields)
+    parsed_args = []
+    for arg in raw_data:
+        # Skip first param, check if if arg starts with acceptable field
+        if len(parsed_args) == 0 or arg.startswith(valid_queries):
+            parsed_args.append(arg)
+        else:
+            # append argument to previous arg and escapse '&'
+            parsed_args[-1] += "%26" + arg.replace(';','%3B')
+    # Rebuild query string and parse as if normal
+    clean_query = "&".join(parsed_args)
+    return parse_qs(clean_query)
